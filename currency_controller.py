7126eb09bd1f0c77257fe0db0543430cb38eed0e
Lugan Costa <luganthierry@hotmail.com>
2024-03-20 01:53:29 -0300

    Creating methods class file

---
+++ currency_controller.py
@@ -0,0 +1,22 @@
+class CurrencyController:
+
+    def __init__(self) -> None:
+        pass
+    
+    @staticmethod
+    def convertStringToFloat(currency):
+
+        currency_replaced = currency.replace(',','.')
+
+        currency_float = float(currency_replaced)
+
+        return currency_float
+
+    @staticmethod
+    def convertCriptoToFloat(cripto):
+
+        cripto_replaced = cripto.replace('R$ ', '').replace('.', '').replace(',', '.')
+
+        cripto_float = float(cripto_replaced)
+
+        return cripto_float
